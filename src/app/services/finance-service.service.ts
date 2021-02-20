99302dd99acf68204306aa6ec1fe36ae203df370
papplevi96 <Realmadrid96>
2021-02-19 21:42:49 -0800

    §

--- src/app/services/finance-service.service.ts
+++ src/app/services/finance-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +10,17 @@ export class FinanceServiceService {
 
   private url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=';
   private endUrl = '&apikey=V03EGS54OMURLU8W';
-  private dataUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol='
+  private dataUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=';
 
   constructor(private http: HttpClient) {
 
   }
 
   search(keyword: string) {
-    return this.http.get<any[]>(`${this.url}${keyword}${this.endUrl}`)
+    return this.http.get<any[]>(`${this.url}${keyword}${this.endUrl}`);
   }
 
   getData(term: string) {
-    return this.http.get<any[]>(`${this.dataUrl}${term}${this.endUrl}`)
+    return this.http.get<any[]>(`${this.dataUrl}${term}${this.endUrl}`);
   }
 }
